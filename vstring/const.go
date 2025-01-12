242306451cab1bcc23161d54fc03cda9f071c53b
flametest <flametest@gmail.com>
2025-01-12 18:22:32 +0800

    feat: add string constant.

--- vstring/const.go
+++ vstring/const.go
@@ -1,1 +1,11 @@
 package vstring
+
+const (
+	Digits         = "0123456789"
+	OctDigits      = "01234567"
+	HexDigits      = Digits + "abcdef" + "ABCDEF"
+	ASCIILowerCase = "abcdefghijklmnopqrstuvwxyz"
+	ASCIIUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ASCIILetter    = ASCIILowerCase + ASCIIUpperCase
+	Symbol         = "!@#$%^&*()\"'+,-./:;<=>?[\\]_`{|}~"
+)

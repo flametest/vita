2f4cd3caf2cfe136433d359cdc21c2ef743e6696
flametest <flametest@gmail.com>
2025-11-02 23:09:38 +0800

    feat: add log

--- vlog/log.go
+++ vlog/log.go
@@ -17,3 +17,27 @@ func InitLogger(app string, level zerolog.Level) {
 	}
 	logger = l
 }
+
+func Debug(args ...interface{}) *zerolog.Event {
+	return logger.Debug(args)
+}
+
+func Info(args ...interface{}) *zerolog.Event {
+	return logger.Info(args)
+}
+
+func Warn(args ...interface{}) *zerolog.Event {
+	return logger.Warn(args)
+}
+
+func Error(args ...interface{}) *zerolog.Event {
+	return logger.Error(args)
+}
+
+func Panic(args ...interface{}) *zerolog.Event {
+	return logger.Panic(args)
+}
+
+func Fatal(args ...interface{}) *zerolog.Event {
+	return logger.Fatal(args)
+}

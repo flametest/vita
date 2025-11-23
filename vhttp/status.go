b6e7e23c31d6842f16be0e9ced4c6c2409c8648f
flametest <flametest@gmail.com>
2025-11-23 13:43:34 +0800

    feat: add error handler middleware.

--- vhttp/status.go
+++ vhttp/status.go
@@ -1,3 +1,7 @@
 package vhttp
 
 type StatusCode int
+
+func (s StatusCode) Int() int {
+	return int(s)
+}
---
+++ vmiddleware/error_handler_middleware.go
@@ -0,0 +1,43 @@
+package vmiddleware
+
+import (
+	"fmt"
+
+	"github.com/flametest/vita/verrors"
+	"github.com/flametest/vita/vhttp"
+	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
+)
+
+func NewErrorResponse(e *verrors.Error) *vhttp.VResponse {
+	return &vhttp.VResponse{
+		Service: e.Service(),
+		Code:    e.ErrCode(),
+		Message: e.ErrMsg(),
+		Error:   e.Error(),
+		Stack:   fmt.Sprintf("%+v", errors.WithStack(e)),
+	}
+}
+
+func ErrorHandleMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			err := next(c)
+			var newErr *verrors.Error
+			if err != nil {
+				if ve, ok := err.(*verrors.Error); ok {
+					newErr = ve
+				} else if ee, ok := err.(*echo.HTTPError); ok {
+					newErr = verrors.New(ee.Code, ee.Error())
+				} else {
+					newErr = verrors.InternalServerError(err.Error())
+				}
+			}
+			if newErr != nil {
+				res := NewErrorResponse(newErr)
+				return c.JSON(newErr.HttpCode().Int(), res)
+			}
+			return err
+		}
+	}
+}

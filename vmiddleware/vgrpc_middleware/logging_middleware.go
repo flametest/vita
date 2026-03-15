27d34735d528ee8a4ebca968a430a183ad5ea259
flametest <flametest@gmail.com>
2026-03-15 14:34:48 +0800

    feat: fix log info.

--- vmiddleware/vgrpc_middleware/logging_middleware.go
+++ vmiddleware/vgrpc_middleware/logging_middleware.go
@@ -12,8 +12,8 @@ func LoggingMiddleware(ctx context.Context, req any, info *grpc.UnaryServerInfo,
 	start := time.Now()
 	m, err := handler(ctx, req)
 	if err != nil {
-		log.Error().Err(err).Msg("[grpc-server] call handler error")
+		log.Error(ctx).Err(err).Msg("[grpc-server] call handler error")
 	}
-	log.Info(ctx).Err(err).Msgf("[grpc-server] latency=%vms, method=%v, from=%v", time.Since(start).Milliseconds(), info.FullMethod, ctx.Value("from"))
+	log.Info(ctx).Msgf("[grpc-server] latency=%vms, method=%v, from=%v", time.Since(start).Milliseconds(), info.FullMethod, ctx.Value("from"))
 	return m, err
 }

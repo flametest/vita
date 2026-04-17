9193f8e53a0796a89a53fa66ea08a2a9f032b7c8
flametest <flametest@gmail.com>
2026-04-17 20:33:20 +0800

    feat: deprecate two method.

--- vstring/strings.go
+++ vstring/strings.go
@@ -1,5 +1,6 @@
 package vstring
 
+// Deprecated: use github.com/samber/lo.Contains instead.
 func StrInList(str string, list []string) bool {
 	for _, v := range list {
 		if v == str {
--- vtool/ptr.go
+++ vtool/ptr.go
@@ -1,5 +1,6 @@
 package vtool
 
+// Deprecated: use github.com/samber/lo.ToPtr instead.
 func Ptr[T any](v T) *T {
 	return &v
 }
